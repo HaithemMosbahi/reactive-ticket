6001706761f5cd3a0c8dda09a9746fc5c98fd36f
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-19 23:59:03 +0100

    add ticket model

--- src/main/java/com/reactive/tickets/reactivetickets/ReactiveTicketsApplication.java
+++ src/main/java/com/reactive/tickets/reactivetickets/ReactiveTicketsApplication.java
@@ -1,7 +1,12 @@
 package com.reactive.tickets.reactivetickets;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @SpringBootApplication
 public class ReactiveTicketsApplication {
@@ -9,4 +14,23 @@ public class ReactiveTicketsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveTicketsApplication.class, args);
 	}
+	
+}
+
+/**
+ * Entity that represents a Ticket
+ * 
+ * @author hmosbahi
+ *
+ */
+@AllArgsConstructor
+@Data
+class Ticket{
+	
+	private String id;
+	private String eventTitle;
+	private String eventDescription;
+	private Date eventDate;
+	
+	
 }
