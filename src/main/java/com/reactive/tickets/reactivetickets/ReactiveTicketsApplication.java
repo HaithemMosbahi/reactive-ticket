0bffd31c9af02f5fae13b7834047edd7dadc88b6
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-19 23:51:29 +0100

    init project structure

---
+++ src/main/java/com/reactive/tickets/reactivetickets/ReactiveTicketsApplication.java
@@ -0,0 +1,12 @@
+package com.reactive.tickets.reactivetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTicketsApplication.class, args);
+	}
+}
