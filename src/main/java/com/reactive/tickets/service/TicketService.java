f7fdcd94fe25062accb345b5737be745b46f6c91
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-21 23:21:55 +0100

    add ticket service

---
+++ src/main/java/com/reactive/tickets/service/TicketService.java
@@ -0,0 +1,14 @@
+package com.reactive.tickets.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service that manage the procedure of purchasing tickets 
+ * 
+ * @author hmosbahi
+ *
+ */
+@Service
+public class TicketService {
+
+}
