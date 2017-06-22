679dc697e422e3804973a32136275e3d8e6854a5
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-22 14:39:35 +0100

    add events service

---
+++ src/main/java/com/reactive/tickets/service/EventsService.java
@@ -0,0 +1,14 @@
+package com.reactive.tickets.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service to fetch upcoming events
+ * 
+ * @author hmosbahi
+ *
+ */
+@Service
+public class EventsService {
+
+}
