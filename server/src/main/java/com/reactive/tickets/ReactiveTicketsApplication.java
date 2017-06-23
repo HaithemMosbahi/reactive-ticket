c836707d50387338ab62fe2119c53fbb251449d2
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-23 14:55:39 +0100

    fetch events from seatgeek API

--- server/src/main/java/com/reactive/tickets/ReactiveTicketsApplication.java
+++ server/src/main/java/com/reactive/tickets/ReactiveTicketsApplication.java
@@ -1,7 +1,11 @@
 package com.reactive.tickets;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.reactive.tickets.service.EventsService;
 
 
 @SpringBootApplication
@@ -10,6 +14,17 @@ public class ReactiveTicketsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveTicketsApplication.class, args);
 	}
+	
+
+  @Bean
+  CommandLineRunner testEvents(EventsService eventService){
+	  return args -> {
+		  eventService.fetchEvents().toStream().forEach(System.out::println);
+	  };
+  }
+	
+	
+	
 	
 }
 
--- server/src/main/java/com/reactive/tickets/service/EventsService.java
+++ server/src/main/java/com/reactive/tickets/service/EventsService.java
@@ -1,6 +1,13 @@
 package com.reactive.tickets.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.reactive.tickets.model.Event;
+
+import reactor.core.publisher.Flux;
 
 /**
  * Service to fetch upcoming events
@@ -10,5 +17,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EventsService {
+	
+	@Value("${events.api.url}")
+	private String apiUrl;
+	@Value("${events.api.key}")
+	private String apiKey;
+	
+	
+	public Flux<JsonNode> fetchEvents(){
+		
+		String eventsUrl = apiUrl+"?client_id="+apiKey;
+		RestTemplate restTemplate=new RestTemplate();
+		JsonNode result = restTemplate.getForObject(eventsUrl, JsonNode.class);
+		
+		return Flux.just(result.get("events"));
+	}
+	
+	
+	
+	
 
 }
